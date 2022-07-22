f328f6862a0d937222b383137250fed10cee12b5
adewale-d-a <adewale-d-a.com>
2022-07-22 16:12:46 +0100

    Image Upload Feature Added

--- backendV2.0/src/dataLayer/TodosStorage.ts
+++ backendV2.0/src/dataLayer/TodosStorage.ts
@@ -1,21 +1,20 @@
 import 'source-map-support/register'
 
 import * as AWS from 'aws-sdk'
-import * as AWSXRay from 'aws-xray-sdk'
+// import * as AWSXRay from 'aws-xray-sdk'
 
-const XAWS = AWSXRay.captureAWS(AWS)
+// const XAWS = AWSXRay.captureAWS(AWS)
 
 export class TodosStorage {
-
   constructor(
-    private readonly s3 = new XAWS.S3({ signatureVersion: 'v4' }),
+    private readonly s3 = new AWS.S3({ signatureVersion: 'v4' }),
     private readonly bucketName = process.env.ATTACHMENTS_S3_BUCKET,
     private readonly urlExpiration = process.env.SIGNED_URL_EXPIRATION
   ) {}
 
   async getAttachmentUrl(attachmentId: string): Promise<string> {
-      const attachmentUrl = `https://${this.bucketName}.s3.amazonaws.com/${attachmentId}`
-      return attachmentUrl
+    const attachmentUrl = `https://${this.bucketName}.s3.amazonaws.com/${attachmentId}`
+    return attachmentUrl
   }
 
   async getUploadUrl(attachmentId: string): Promise<string> {
@@ -26,5 +25,4 @@ export class TodosStorage {
     })
     return uploadUrl
   }
-
 }
--- client/src/config.ts
+++ client/src/config.ts
@@ -1,4 +1,4 @@
-const apiId = 'wbb82qlumj'
+const apiId = 'irya0foxf9'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
